40ad26403167db61d146f6b905ffe512a6d23b14
Arul Saxena <saxena@ucsb.edu>
2023-12-05 10:42:04 -0800

    as - update ChatContainer to match refactor

--- frontend/src/main/components/Chat/ChatContainer.js
+++ frontend/src/main/components/Chat/ChatContainer.js
@@ -1,8 +1,11 @@
 import React, {useState} from "react";
 import ChatPanel from "./ChatPanel";
 import ChatToggle from "./ChatToggle";
+import { useCurrentUser } from "main/utils/currentUser";
 
 const ChatContainer = ({commonsId}) => {
+    const { data: currentUser } = useCurrentUser();
+
     const chatContainerStyle = {
         width: '550px',
         position: 'fixed',
@@ -18,7 +21,7 @@ const ChatContainer = ({commonsId}) => {
 
     return (
         <div style={chatContainerStyle} data-testid="playpage-chat-div">
-            {!!isChatOpen && <ChatPanel commonsId={commonsId}/>}
+            {!!isChatOpen && <ChatPanel commonsId={commonsId} currentUser={currentUser}/>}
             <ChatToggle isChatOpen={isChatOpen} onClick={toggleChatWindow} />
         </div>
     );
